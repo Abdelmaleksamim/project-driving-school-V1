c40e90c7f920e970fb723e88fe23d1bd924fe4e1
unknown <samimbahmad09@gmail.com>
2025-11-20 18:19:14 +0100

    last version of frontend 20112025

--- auto_ecole_backend/hash.js
+++
@@ -1,8 +0,0 @@
-const bcrypt = require('bcrypt');
-
-const plainPassword = 'Samim1234';
-
-bcrypt.hash(plainPassword, 10, (err, hash) => {
-    if (err) throw err;
-    console.log('Hashed password: ', hash);
-});
